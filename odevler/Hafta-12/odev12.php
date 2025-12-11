a5d97c2dc362b630eefcbf91b04be9ccb8c8fd70
Kadir <dragonscript892@gmail.com>
2025-12-11 17:59:33 +0300

    Create odev12.php

---
+++ odevler/Hafta-12/odev12.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ödev 12r</title>
+</head>
+<body>
+    <h1>1-100 Arası Tek Sayılar</h1>
+    <p>
+        <?php
+        for ($i = 1; $i <= 100; $i += 2) {
+            echo $i . " ";
+        }
+        ?>
+    </p>
+</body>
+</html>
