454dbf8ba335fbb32c0729a3a02585f46aff99e7
Kadir <dragonscript892@gmail.com>
2026-01-03 19:13:02 +0300

    Create Footer.jsx

---
+++ Final Ödevi/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 IP Vision - Web Programcılığı Final Ödevi</p>
+      <small>Powered by <a href="https://ip-api.com" target="_blank" rel="noreferrer">ip-api</a></small>
+    </footer>
+  );
+}
