41105ecb87d3ea73db0d86a4c74cf473234f4345
Kadir <dragonscript892@gmail.com>
2026-01-03 20:06:11 +0300

    Delete Final Ödevi directory

--- Final Ödevi/components/Header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <h1>IP <span className="neon-text">VISION</span></h1>
-      <p>Dünya üzerindeki herhangi bir IP adresini anında izleyin.</p>
-    </header>
-  );
-}
