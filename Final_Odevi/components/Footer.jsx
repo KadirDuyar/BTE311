b965d93908898da03317aab1f9b40307965b46e9
Kadir <dragonscript892@gmail.com>
2026-01-11 19:39:39 +0300

    Update Footer.jsx

--- Final_Odevi/components/Footer.jsx
+++ Final_Odevi/components/Footer.jsx
@@ -2,7 +2,6 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2026 IP Sorgualama - Web Tabanlı Programlama Final Ödevi</p>
-      <small>Powered by <a href="https://ip-api.com" target="_blank" rel="noreferrer">ip-api</a></small>
     </footer>
   );
 }
