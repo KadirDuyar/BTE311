3713a1fd1c92431eccd14cfe1abb2b926992db27
Kadir <dragonscript892@gmail.com>
2026-01-11 19:42:00 +0300

    Update Header.jsx

--- Final_Odevi/components/Header.jsx
+++ Final_Odevi/components/Header.jsx
@@ -1,8 +1,7 @@
 export default function Header() {
   return (
     <header className="header">
-      <h1>IP <span className="neon-text">VISION</span></h1>
-      <p>Dünya üzerindeki herhangi bir IP adresini anında izleyin.</p>
+      <h1>IP <span className="neon-text">SORGULAMA</span></h1>
     </header>
   );
 }
