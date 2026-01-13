340158a401c6989c09bf52be9a35f07c126accc7
Kadir <dragonscript892@gmail.com>
2026-01-13 21:12:13 +0300

    Update Footer.jsx

--- Final_Odevi/components/Footer.jsx
+++ Final_Odevi/components/Footer.jsx
@@ -2,6 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2026 IP Sorgualama - Web Tabanlı Programlama Final Ödevi</p>
+      <strong> KADİR DUYAR</strong>
     </footer>
   );
 }
